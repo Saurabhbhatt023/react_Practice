0bf9192fcb160a764e6f0396fe726b15cf3d6b78
Saurabh bhatt <85731069+Saurabhbhatt023@users.noreply.github.com>
2024-08-18 13:44:33 +0530

    Add files via upload

---
+++ Structure.js
@@ -0,0 +1,17 @@
+my-ecommerce-app/
+├── public/
+│   └── index.html
+├── src/
+│   ├── components/
+│   │   ├── Cart.js
+│   │   ├── Navbar.js
+│   │   └── ProductList.js
+│   ├── context/
+│   │   └── CartContext.js
+│   ├── pages/
+│   │   ├── CartPage.js
+│   │   └── HomePage.js
+│   ├── App.js
+│   ├── index.css
+│   └── index.js
+└── package.json
